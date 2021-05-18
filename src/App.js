99a4c157c12a02583492f95d589bd5ec35de5c84
Teyanna Earle <teyonna.earle92@gmail.com>
2021-05-18 15:59:07 -0400

    Set up nav

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import Navbar from "./Components/Navbar"
+import { Route, Switch } from "react-router-dom"
+import HomePage from "./Components/HomePage"
+import About from "./Components/About"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={HomePage}></Route>
+          <Route path="/about" component={About}></Route>
+        </Switch>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App

b203ce6cfd509392607f495fd5139df344ed591c
Teyanna Earle <teyonna.earle92@gmail.com>
2021-05-18 18:38:40 -0400

    Add Nav Css

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Link } from "react-router-dom"
+import "./Navbar.css"
+import Logo from "../Assets/youtube-50.png"
+import About from "../Assets/info-2-50.png"
 
 function Navbar() {
     return (
        <nav>
-           <Link to="/">YouTube</Link>
-           <Link to="/about">About</Link>
+           <Link to="/"><img src={Logo}/></Link>
+           <Link to="/about"><img src={About}/></Link>
        </nav>
     )
 }

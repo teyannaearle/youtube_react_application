9d8e97010f58324259f07e8b234cae85f3c9c2ea
Angelina Ebreo <angelinaebreo@gmail.com>
2021-05-22 12:00:14 -0400

    update firebase.js

--- src/Components/firebase.js
+++ src/Components/firebase.js
@@ -1,8 +1,8 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import firebase from "firebase"
+// 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC1UNppIcOo8fMp4b5L29zFAD2sRr9Iurk",
     authDomain: "react--314117.firebaseapp.com",
     databaseURL: "https://react--314117-ca55d.firebaseio.com",
     projectId: "react-youtube-314117",

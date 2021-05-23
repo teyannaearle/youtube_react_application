0b588fb91f989926b4692407e1752df15e3c6673
Teyanna Earle <teyonna.earle92@gmail.com>
2021-05-23 17:22:32 -0400

    Change heart postion on like/dislike buttons

--- src/Components/Dislikebutton.js
+++ src/Components/Dislikebutton.js
@@ -10,7 +10,7 @@ function DislikeBtn(props) {
     <button>
       <img src={Dislike} alt="dislike" />
       <br />
-      {dislikes} {dislikes === 1 ? "dislike 💔" : "dislikes 💔"}
+      💔{dislikes} {dislikes === 1 ? "dislike " : "dislikes"}     
     </button>
   );
 }
